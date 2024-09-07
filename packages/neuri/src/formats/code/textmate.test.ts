fc2c4c88281307ead352e2cafef2b87d72521897
Neko Ayaka <neko@ayaka.moe>
2024-09-07 17:22:24 +0800

    feat: support to directly extract with langauge
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages/neuri/src/formats/code/textmate.test.ts
+++ packages/neuri/src/formats/code/textmate.test.ts
@@ -1,13 +1,53 @@
-import { writeFile } from 'node:fs/promises'
-import { describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
-import { extractByTextMateGrammar } from './textmate'
-
-const vueCode = `<script setup lang=ts>import ProductCard from '~/components/ProductCard.vue'</script><template> <div> <ProductCard :prop-a="1" />  </div> </template><style scoped> .product-card {  background-color: red;  } </style>`
+import { extractByTextMateGrammar, extractGolangCode, extractJavaScriptCode, extractRustCode, extractTsxCode, extractTypeScriptCode, extractVueCode } from './textmate'
 
 describe('extractByTextMateGrammar', () => {
-  it('should extract tokens from textmate grammar', async () => {
-    const res = await extractByTextMateGrammar('vue', `你好\n你好，这个是一个测试 \`\`\`vue${vueCode}\`\`\``)
-    await writeFile('test.json', JSON.stringify(res, null, 2))
+  it('should extract vue tokens from textmate grammar', async () => {
+    const vueCode = `<script setup lang=ts>import ProductCard from '~/components/ProductCard.vue'</script><template> <div> <ProductCard :prop-a="1" />  </div> </template><style scoped> .product-card {  background-color: red;  } </style>`
+
+    const res = await extractByTextMateGrammar('vue', `Sure, this is the code \`\`\`vue${vueCode}\`\`\` whatever ends here`)
+    const code = extractVueCode(res, vueCode)
+    expect(code).toBe(vueCode)
+  })
+
+  it('should extract tsx tokens from textmate grammar', async () => {
+    const tsxCode = `import React from 'react';import { ProductCard } from './ProductCard';export const App = () => {  return <ProductCard propA={1} />;};`
+
+    const res = await extractByTextMateGrammar('tsx', `Sure, this is the code \`\`\`tsx${tsxCode}\`\`\` whatever ends here`)
+    const code = extractTsxCode(res, tsxCode)
+    expect(code).toBe(tsxCode)
+  })
+
+  it('should extract golang tokens from textmate grammar', async () => {
+    const golangCode = `package mainimport "fmt"func main() {  fmt.Println("Hello, World!")}`
+
+    const res = await extractByTextMateGrammar('go', `Sure, this is the code \`\`\`go${golangCode}\`\`\` whatever ends here`)
+    const code = extractGolangCode(res, golangCode)
+    expect(code).toBe(golangCode)
+  })
+
+  it('should extract js tokens from textmate grammar', async () => {
+    const jsCode = `console.log('Hello, World!')`
+
+    const res = await extractByTextMateGrammar('js', `Sure, this is the code \`\`\`js${jsCode}\`\`\` whatever ends here`)
+    const code = extractJavaScriptCode(res, jsCode)
+    expect(code).toBe(jsCode)
+  })
+
+  it('should extract ts tokens from textmate grammar', async () => {
+    const tsCode = `type Product = {  name: string;  price: number;};const product: Product = {  name: 'Product',  price: 100};`
+
+    const res = await extractByTextMateGrammar('ts', `Sure, this is the code \`\`\`js${tsCode}\`\`\` whatever ends here`)
+    const code = extractTypeScriptCode(res, tsCode)
+    expect(code).toBe(tsCode)
+  })
+
+  it('should extract rust tokens from textmate grammar', async () => {
+    const rustCode = `fn main() {  println!("Hello, World!");}`
+
+    const res = await extractByTextMateGrammar('rust', `Sure, this is the code \`\`\`rust${rustCode}\`\`\` whatever ends here`)
+    const code = extractRustCode(res, rustCode)
+    expect(code).toBe(rustCode)
   })
 })
--- packages/neuri/src/formats/code/textmate.ts
+++ packages/neuri/src/formats/code/textmate.ts
@@ -57,10 +57,129 @@ export interface ITokenizeLineResult {
   readonly stoppedEarly: boolean
 }
 
+/**
+ * Extract code from a text using TextMate grammar.
+ *
+ * @param lang The language to extract code from.
+ * @param extractFrom The text to extract code from.
+ * @returns The extracted code.
+ */
 export async function extractByTextMateGrammar(lang: BundledLanguage, extractFrom: string): Promise<ITokenizeLineResult> {
   await ensureLoadWASM()
   await ensureShikiInternal()
 
   const res = shiki.getLanguage(lang).tokenizeLine(extractFrom, null, 0)
   return res
+}
+
+function extractCode(result: ITokenizeLineResult, text: string, scopes: string[]): string {
+  let code = ''
+  let lastEndIndex = 0
+
+  result.tokens.forEach((token) => {
+    const isTargetScope = token.scopes.some(scope =>
+      scopes.some(targetScope =>
+        scope === targetScope || scope.startsWith(`${targetScope} `),
+      ),
+    )
+
+    if (isTargetScope) {
+      code += text.slice(lastEndIndex, token.endIndex)
+      lastEndIndex = token.endIndex
+    }
+  })
+
+  return code.trim()
+}
+
+/**
+ * Extract Vue code from a text using TextMate grammar.
+ *
+ * @param result The result of tokenization.
+ * @param text The text to extract Vue code from.
+ * @returns The extracted Vue code.
+ */
+export function extractVueCode(result: ITokenizeLineResult, text: string): string {
+  let vueCode = ''
+  let isVueBlock = false
+  let lastEndIndex = 0
+
+  result.tokens.forEach((token) => {
+    const isVueScope = token.scopes.some(scope =>
+      scope === 'source.vue'
+      || scope.startsWith('source.vue ')
+      || scope.startsWith('text.html.vue'),
+    )
+
+    if (isVueScope && !isVueBlock) {
+      isVueBlock = true
+      lastEndIndex = token.startIndex
+    }
+    else if (!isVueScope && isVueBlock) {
+      vueCode += text.slice(lastEndIndex, token.startIndex)
+      isVueBlock = false
+    }
+
+    if (isVueBlock) {
+      vueCode += text.slice(lastEndIndex, token.endIndex)
+      lastEndIndex = token.endIndex
+    }
+  })
+
+  return vueCode.trim()
+}
+
+/**
+ * Extract TSX code from a text using TextMate grammar.
+ *
+ * @param result The result of tokenization.
+ * @param text The text to extract TypeScript code from.
+ * @returns The extracted TypeScript code.
+ */
+export function extractTsxCode(result: ITokenizeLineResult, text: string): string {
+  return extractCode(result, text, ['source.tsx', 'source.tsx ', 'source.js', 'source.js '])
+}
+
+/**
+ * Extract JSX code from a text using TextMate grammar.
+ *
+ * @param result The result of tokenization.
+ * @param text The text to extract JavaScript code from.
+ * @returns The extracted JavaScript code.
+ */
+export function extractGolangCode(result: ITokenizeLineResult, text: string): string {
+  return extractCode(result, text, ['source.go', 'source.go '])
+}
+
+/**
+ * Extract JavaScript code from a text using TextMate grammar.
+ *
+ * @param result The result of tokenization.
+ * @param text The text to extract JavaScript code from.
+ * @returns The extracted JavaScript code.
+ */
+export function extractJavaScriptCode(result: ITokenizeLineResult, text: string): string {
+  return extractCode(result, text, ['source.js', 'source.js '])
+}
+
+/**
+ * Extract TypeScript code from a text using TextMate grammar.
+ *
+ * @param result The result of tokenization.
+ * @param text The text to extract TypeScript code from.
+ * @returns The extracted TypeScript code.
+ */
+export function extractTypeScriptCode(result: ITokenizeLineResult, text: string): string {
+  return extractCode(result, text, ['source.ts', 'source.ts '])
+}
+
+/**
+ * Extract Rust code from a text using TextMate grammar.
+ *
+ * @param result The result of tokenization.
+ * @param text The text to extract Rust code from.
+ * @returns The extracted Rust code.
+ */
+export function extractRustCode(result: ITokenizeLineResult, text: string): string {
+  return extractCode(result, text, ['source.rust', 'source.rust '])
 }
