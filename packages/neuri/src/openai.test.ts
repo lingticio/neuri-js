3c05a93bfc2d475fa06f27b9a46b145a9943e946
Neko Ayaka <neko@ayaka.moe>
2024-11-29 11:58:04 +0800

    refactor: better stream api

--- packages/neuri/src/openai.test.ts
+++ packages/neuri/src/openai.test.ts
@@ -1,7 +1,9 @@
+import { env } from 'node:process'
+import OpenAI from 'openai'
+
 import { describe, expect, it } from 'vitest'
 import { object, string } from 'zod'
-
-import { defineToolFunction, newTestInvokeContext, toolFunction } from './openai'
+import { defineToolFunction, messages, newTestInvokeContext, stream, type StreamChunk, system, toolFunction, user } from './openai'
 
 describe('neuri/openai', async () => {
   it('it works', async () => {
@@ -18,5 +20,53 @@ describe('neuri/openai', async () => {
 
     const name = await tf.func(newTestInvokeContext({ name: 'name' }))
     expect(name).toBe('name')
+  })
+})
+
+describe('stream', async () => {
+  it('it works', async () => {
+    const openai = new OpenAI({
+      apiKey: env.OPENAI_API_KEY,
+      baseURL: env.OPENAI_API_BASEURL,
+    })
+
+    const res = await stream({
+      openAI: openai,
+      options: {
+        model: 'openai/gpt-3.5-turbo',
+        messages: messages(
+          system('You are a helpful assistant.'),
+          user('What is the meaning of life?'),
+        ),
+      },
+    })
+
+    const readText = async () => {
+      const readTextParts: string[] = []
+      for await (const text of res.textStream()) {
+        readTextParts.push(text)
+      }
+
+      return readTextParts
+    }
+
+    const readChunk = async () => {
+      const readChunkParts: StreamChunk[] = []
+      for await (const chunk of res.chunkStream()) {
+        readChunkParts.push(chunk)
+      }
+
+      return readChunkParts
+    }
+
+    const [readTextParts, readChunks] = await Promise.all([readText(), readChunk()])
+    const textCompletions = readTextParts.join('')
+    const textCompletionsFromChunks = readChunks.map(i => i.textPart()).join('')
+
+    expect(readTextParts.length).toBeGreaterThan(0)
+    expect(readChunks.length).toBeGreaterThan(0)
+    expect(textCompletions).toBe(textCompletionsFromChunks)
+    expect(await res.text()).toBe(textCompletions)
+    expect((await res.chunks()).length).toBe(readChunks.length)
   })
 })
--- packages/neuri/src/openai.ts
+++ packages/neuri/src/openai.ts
@@ -1,6 +1,6 @@
 import type { Infer, Schema } from '@typeschema/main'
-import type OpenAI from 'openai'
 
+import type OpenAI from 'openai'
 import { toJSONSchema } from '@typeschema/main'
 
 /**
@@ -52,6 +52,19 @@ export interface StreamResponse {
    */
   text: () => Promise<string>
   /**
+   *
+   * Get the completion response as a stream of text.
+   *
+   * @returns The completion response as a stream of text.
+   */
+  textStream: () => AsyncGenerator<string, void, unknown>
+  /**
+   * Get the completion response as a stream of chunks.
+   *
+   * @returns The completion response as a stream of chunks.
+   */
+  chunkStream: () => AsyncGenerator<StreamChunk, void, unknown>
+  /**
    * Get the completion response as a stream of chunks.
    *
    * @returns The completion response as a stream of chunks.
@@ -65,12 +78,35 @@ export interface StreamResponse {
    * @param options
    * @returns
    */
-  toReadableStream: () => AsyncGenerator<StreamChunk, void, unknown>
+  toReadableStream: () => AsyncGenerator<Uint8Array, void, unknown>
 }
 
-interface PipeHook {
-  onChunk?: (chunk: StreamChunk) => void
+interface PipeHook<F, T> {
+  onValue?: (value: F) => void
+  onChunk?: (chunk: T) => void
   onDone?: () => void
+}
+
+async function* asyncIteratorFromReadableStream<T, F = Uint8Array>(res: ReadableStream<F>, func: (value: F) => Promise<T>, hooks?: PipeHook<F, T>[]): AsyncGenerator<T, void, unknown> {
+  // reactjs - TS2504: Type 'ReadableStream<Uint8Array>' must have a '[Symbol.asyncIterator]()' method that returns an async iterator - Stack Overflow
+  // https://stackoverflow.com/questions/76700924/ts2504-type-readablestreamuint8array-must-have-a-symbol-asynciterator
+  const reader = res.getReader()
+  try {
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) {
+        hooks?.forEach(i => i.onDone?.())
+        return
+      }
+
+      const chunk = await func(value)
+      hooks?.forEach(i => i.onChunk?.(chunk))
+      yield chunk
+    }
+  }
+  finally {
+    reader.releaseLock()
+  }
 }
 
 /**
@@ -82,63 +118,47 @@ interface PipeHook {
  * @returns The completion response stream.
  */
 export async function stream(params: {
-  options: OpenAI.Chat.Completions.ChatCompletionCreateParamsStreaming
+  options: Omit<OpenAI.Chat.Completions.ChatCompletionCreateParamsStreaming, 'stream'>
   openAI: OpenAI
 }): Promise<StreamResponse> {
-  const res = await params.openAI.chat.completions.create(params.options)
-  const hooks: PipeHook[] = []
+  const res = await params.openAI.chat.completions.create({ ...params.options, stream: true, stream_options: { include_usage: true } })
+
+  const [fullStream, rawStream] = res.tee()
+  const [textStream, chunkStream] = fullStream.tee()
+
+  const textStreamHooks: PipeHook<Uint8Array, string>[] = []
   const accumulatedText = new Promise<string>((resolve) => {
-    const chunks: StreamChunk[] = []
-    hooks.push({
-      onChunk: (chunk) => {
-        chunks.push(chunk)
-      },
-      onDone: () => {
-        resolve(chunks.map(i => i.textPart()).join(''))
-      },
+    const chunks: string[] = []
+    textStreamHooks.push({
+      onChunk: (chunk) => { chunks.push(chunk) },
+      onDone: () => { resolve(chunks.join('')) },
     })
   })
+
+  const chunkStreamHooks: PipeHook<Uint8Array, StreamChunk>[] = []
   const accumulatedChunks = new Promise<StreamChunk[]>((resolve) => {
     const chunks: StreamChunk[] = []
-    hooks.push({
-      onChunk: (chunk) => {
-        chunks.push(chunk)
-      },
-      onDone: () => {
-        resolve(chunks)
-      },
+    chunkStreamHooks.push({
+      onChunk: (chunk) => { chunks.push(chunk) },
+      onDone: () => { resolve(chunks) },
     })
   })
-
-  /*
-      reactjs - TS2504: Type 'ReadableStream<Uint8Array>' must have a '[Symbol.asyncIterator]()' method that returns an async iterator - Stack Overflow
-      https://stackoverflow.com/questions/76700924/ts2504-type-readablestreamuint8array-must-have-a-symbol-asynciterator
-    */
-  async function* pipeAsGenerator(res: ReadableStream<Uint8Array>, hooks?: PipeHook[]): AsyncGenerator<StreamChunk, void, unknown> {
-    const reader = res.getReader()
-    try {
-      while (true) {
-        const { done, value } = await reader.read()
-        if (done) {
-          hooks?.forEach(i => i.onDone?.())
-          return
-        }
-
-        const chunk = newStreamChunk(value, parseChunkFromBuffer(value))
-        hooks?.forEach(i => i.onChunk?.(chunk))
-        yield chunk
-      }
-    }
-    finally {
-      reader.releaseLock()
-    }
-  }
 
   return {
     response: async () => res,
     text: () => accumulatedText,
     chunks: () => accumulatedChunks,
-    toReadableStream: () => (pipeAsGenerator(res.toReadableStream())),
+    textStream: () => asyncIteratorFromReadableStream(
+      textStream.toReadableStream(),
+      async (value: Uint8Array) => newStreamChunk(value, parseChunkFromBuffer(value)).textPart(),
+      textStreamHooks,
+    ),
+    chunkStream: () => asyncIteratorFromReadableStream(
+      chunkStream.toReadableStream(),
+      async (value: Uint8Array) => newStreamChunk(value, parseChunkFromBuffer(value)),
+      chunkStreamHooks,
+    ),
+    toReadableStream: () => rawStream.toReadableStream() as unknown as AsyncGenerator<Uint8Array, void, unknown>,
   } satisfies StreamResponse
 }
 
